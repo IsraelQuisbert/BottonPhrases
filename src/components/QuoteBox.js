b1b5f42f9c466a9bcb6a489d02becfea4a658ece
Israel Quisbert <capricos999@gmail.com>
2022-05-02 20:20:39 -0300

    todo

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,39 @@
+import React from "react";
+import quotes from "../quotes.json"
+import { useState } from "react";
+import Button from "./button";
+
+const colors = ["red", "blue", "aqua", "lemon", "yellowgreen", "blueviolet", "green"]
+
+const Card = () => {
+
+    console.log(quotes);
+
+
+    const randomPhrase = Math.floor (Math.random() * quotes.length )
+    
+    const [ phrase, setPhrase ] = useState(randomPhrase)
+    
+    const change = () =>{ 
+        
+        const randomPhrase = Math.floor (Math.random() * quotes.length )
+        
+        setPhrase ( randomPhrase) }
+
+    const randomColor = Math.floor (Math.random() * colors.length )
+    document.body.style = `background: ${colors[randomColor]}`
+
+
+    return (
+        <div style={{color: colors[randomColor]}}>
+            <h1> <i class="fa-solid fa-check-double"></i> {quotes[phrase].quote}</h1>
+            <h3 className="autor">{quotes[phrase].author}</h3>
+            <button style={{backgroundColor: colors[randomColor]}} onClick={change}>
+                other
+            </button>
+           
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = () => {
+
+
+    return (
+        <div>
+            {/* intente pero no pude lograr colocar el botton  qie sea funcional*/}
+        </div>
+    );
+};
+
+export default Button
